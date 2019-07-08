1b353f1b8d29f84826f78efefb41ba36520d0d1e
MatthewChen37 <epicninjasauce@gmail.com>
2019-07-08 13:15:03 -0400

    Added more functions

--- mmsfitting_matt.py
+++ mmsfitting_matt.py
@@ -15,20 +15,24 @@ import scipy
 
 #Turns (x,y,z) coordinates into (r, theta, z) coordinates 
 def RectToCylindrical(RectArray):   
+    RectArray = normalize(RectArray)
     row, column = len(RectArray), len(RectArray[0]) #Gets the Height and Width of Array for conversion
     CylindArray = [[0 for x in range(column)] for y in range[row]] #Initializes the return array as empty
     for i in range(row): #Each row is a 3 dimensional vector, this loops goes down each row
         x = RectArray[i][0]
         y = RectArray[i][2]
         r = math.sqrt((x ^ 2) + (y ^ 2)) # finds radial distance or B-Azimuthal
-        theta = math.atan(y/x) #Finds angle theta based off of x and y vectors, theta is in RADIANS
+        if x > 0:
+            theta = math.atan(y/x) #Finds angle theta based off of x and y vectors, theta is in RADIANS
+        elif x == 0:
+            theta = 0
+        else:
+            theta = -math.asin(y/r) + math.pi
         CylindArray[i][0] = r #Store radial distance in first position --> Azimuthal
         CylindArray[i][2] = theta #Stores angular direction in second position --> Azimuthal 
         CylindArray[i][1] = RectArray[i][1] #Z position left untouched, stored in 3rd position --> Axial Coordinate
      
     return CylindArray 
-
-
 
 def getAzimuthal(maxVar, minVar):
     B_azi = math.sqrt((maxVar ^ 2) + (minVar ^ 2))
@@ -66,14 +70,17 @@ def normalize(oldArray): #makes all vectors a ratio of the largest magnitude vec
     
 
 def chisquared1(RectArray): #first chi-squared test as defined by Smith et al., 2017
-    #CylindArray = RectToCylindrical(RectArray)
+    CylindArray = RectToCylindrical(RectArray) 
+    #not sure if this is correct -- made a lot of assumptions
     impactParameter = 0
     minChiSquared = 0
     chiSquaredValue = 0 
     for y in range(0, 0.95, 0.01):
         B_axial, B_azimuthal = modelFluxRope(y)
         for x in range(len(RectArray)):
-            chiSquaredValue += (((getAzimuthal(RectArray[x][0], RectArray[x][2]) - B_azimuthal) ^ 2) + ((RectArray[0][1] - B_axial) ^ 2))
+            chiSquaredValue += ((CylindArray[x][0]- B_azimuthal) ^ 2) + ((CylindArray[0][1] - B_axial) ^ 2)
+            '''for  the model equation does it only give magnitudes in the cylindrical coordinate system?
+               in the paper is the chi squared only comparing the magnitudes of the magnetic fields in the azimuthal direction?'''
         chiSquaredValue = chiSquaredValue / len(RectArray)
         if y == 0:
             impactParameter = 0
@@ -85,7 +92,9 @@ def chisquared1(RectArray): #first chi-squared test as defined by Smith et al., 
         return "event rejected"
     else:
         return minChiSquared, impactParameter 
-    
+
+
+def chiSquared2()
     
             
         
